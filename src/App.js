3eb4983a929a6727079e1a6a0d505cbd998a4d22
Naresh28696 <nareshpatil.nrp@gmail.com>
2020-03-29 17:32:29 +0530

    [Naresh] Modify. remove unusable route from the file

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { Switch } from 'react-router-dom'
 import Registration from './components/registration'
 import Login from './components/login'
 import Dashboard from './components/dashboard'
-import Test from './components/test'
 import ForgotPassword from './components/forgotPassword'
 import FundooServices from './components/fundooSevices'
 
@@ -18,7 +17,6 @@ class App extends React.Component {
           <Route path='/registration' component={Registration} />
           <Route path='/login' component={Login} />
           <Route path='/dashboard' component={Dashboard} />
-          <Route path='/test' component={Test} />
           <Route path='/forgotpassword' component={ForgotPassword} />
           <Route path="/fundooServices" component={FundooServices} />
         </Switch>
