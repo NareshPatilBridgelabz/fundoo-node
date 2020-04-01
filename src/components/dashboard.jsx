192d15c19ac8c5f59e0d74bff50d73fa8a9e228c
Naresh28696 <nareshpatil.nrp@gmail.com>
2020-04-01 17:11:46 +0530

    [Naresh] Refactore. add SubNote functionality

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -24,7 +24,16 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
 import ColorLensIcon from '@material-ui/icons/ColorLens';
 import AddPhotoAlternateIcon from '@material-ui/icons/AddPhotoAlternate';
-
+import AddAlertIcon from '@material-ui/icons/AddAlert';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import PlaceIcon from '@material-ui/icons/Place';
+import Note from './notes'
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import TextField from '@material-ui/core/TextField';
+import {getUserNote} from '../services/noteServices'
+import {setUserNote} from '../services/noteServices'
+import {deleteUserNote} from '../services/noteServices'
 // import Appbar from './appBar'
 import AcUnitIcon from '@material-ui/icons/AcUnit'
 // import Header from './dashbord/header'
@@ -41,9 +50,45 @@ class Dashboard extends Component {
       reminderMenuAnchor: null,
       reminderMenuOpen: false,
       MoreMenuAnchor: null,
-      MoreMenuOpen: false
+      MoreMenuOpen: false,
+      noteCardBackDisplay: "none",
+      title: '',
+      description: '',
+      allNotes: [],
+      displayReminder: "",
+      displayDatePick: "none",
+      timing: [
+        {
+          value: 'morning8:00AM',
+          label: 'morning     8:00 AM'
+        }, {
+          value: 'afternoon1:00PM',
+          label: 'afternoon   1:00 PM'
+        }, {
+          value: 'evening4:00PM',
+          label: 'evening     4:00 PM'
+        }, {
+          value: 'night8:00PM',
+          label: 'night       8:00 PM'
+        }
+      ],
+      time: ""
     }
+
+    this.userNoteRefresh()
+    deleteUserNote().then(response => {
+      console.log(response)
+    })
+
   }
+  userNoteRefresh = () => {
+    getUserNote().then(response => {
+      if(response.data.data.data){
+        this.setState({allNotes:response.data.data.data})
+      }
+    })
+  }
+  
   handleClick = event => {
     this.setState({
       menuAnchor: event.currentTarget,
@@ -73,6 +118,7 @@ class Dashboard extends Component {
   }
   handleLogout = () => {
     localStorage.removeItem('token')
+    localStorage.removeItem('userDetails')
     this
       .props
       .history
@@ -93,8 +139,60 @@ class Dashboard extends Component {
       MoreMenuOpen: !this.state.MoreMenuOpen
     })
   }
+  addNote = (event) => {
+    if(this.state.title != "" || this.state.description != ""){
+      
+      
+      const form_data = new FormData();
+      form_data.append('title', this.state.title);
+      form_data.append('description', this.state.description);
+
+      setUserNote(form_data).then(response => {
+        console.log(response)
+      })
+      this.userNoteRefresh()
+      this.setState({title:""})
+      this.setState({description:""})
+    }
+  }
+  onChangeTitle = (event) => {
+    this.setState({title: event.currentTarget.value})
+  }
+  onChangeNote = (event) => {
+    this.setState({description: event.currentTarget.value})
+  }
+  onFocusTitle = () => {
+    // alert("sdsdsd");
+    this.setState({
+      noteCardBackDisplay: this.state.noteCardBackDisplay === ""
+        ? "none"
+        : ""
+    })
+  }
+  onClickCheckList = () => {}
+  clickPickDate = () => {
+    this.setState({
+      displayReminder: this.state.displayReminder === ""
+        ? "none"
+        : ""
+    })
+    this.setState({
+      displayDatePick: this.state.displayDatePick === ""
+        ? "none"
+        : ""
+    })
+  }
+  handleChangeTime = (event) => {
+    console.log(event.currentTarget, event.currentTarget.value);
+    console.log(event.currentTarget.dataset.value)
+    this.setState({time: event.currentTarget.dataset.value})
+  }
 
   render() {
+    // let allObj = this   .state   .allNotes   .map(allnote => {     if
+    // (allnote.isArchived === false && allnote.isDeleted === false &&
+    // allnote.isPined === false) {       return (<Note         />)     }     return
+    // null   })
     return (
       <div>
         <div className='headerbar'>
@@ -191,27 +289,36 @@ class Dashboard extends Component {
               <Card >
                 <CardContent>
                   <Typography color="textSecondary">
-                    <InputBase placeholder="Title"/>
+                    <InputBase
+                      placeholder="Title"
+                      value={this.state.title}
+                      onChange={this.onChangeTitle}
+                      onClick={this.onFocusTitle}/>
+                    <CheckBoxIcon id="checkList" onClick={this.onClickCheckList}/>
                   </Typography>
-                  <Typography >
+                  <Typography
+                    style={{
+                    display: this.state.noteCardBackDisplay
+                  }}>
                     <InputBase
                       placeholder="Take a note..."
-                      inputProps={{
-                      'aria-label': 'naked'
-                    }}/>
+                      value={this.state.description}
+                      onChange={this.onChangeNote}/>
+                    <div className="listItem"></div>
                   </Typography>
 
                 </CardContent>
-                <CardActions>
+                <CardActions
+                  style={{
+                  display: this.state.noteCardBackDisplay
+                }}>
                   <div className="cardActions">
                     <div className="card_buttonsLeft">
                       <IconButton onClick={this.remiderHandler}>
-                        <SettingsIcon/>
+                        <AddAlertIcon/>
                       </IconButton>
                       <Menu
-                        style={{
-                        top: "50px"
-                      }}
+                        className="reminderMenu"
                         anchorOrigin={{
                         vertical: 'bottom',
                         horizontal: 'center'
@@ -224,10 +331,48 @@ class Dashboard extends Component {
                         keepMounted
                         open={this.state.reminderMenuOpen}
                         onClose={this.remiderHandler}>
-                        <MenuItem>Reminder</MenuItem>
-                        <MenuItem>Pick date & time</MenuItem>
-                        <MenuItem>Pick place
-                        </MenuItem>
+
+                        <div
+                          style={{
+                          display: this.state.displayReminder
+                        }}>
+                          <MenuItem component="h2">Reminder</MenuItem>
+                          <MenuItem>Later today</MenuItem>
+                          <MenuItem>Tomorrow</MenuItem>
+                          <MenuItem onClick={this.clickPickDate}><WatchLaterIcon fontSize=" 0.90rem"/>Pick date & time</MenuItem>
+
+                        </div>
+                        <div
+                          id="datePickBox"
+                          style={{
+                          display: this.state.displayDatePick
+                        }}>
+
+                          <Typography onClick={this.clickPickDate}><ArrowBackIcon/>Pick Date & Time</Typography>
+                          <TextField
+                            id="date"
+                            type="date"
+                            defaultValue="2017-05-24"
+                            InputLabelProps={{
+                            shrink: true
+                          }}/>
+                          <TextField
+                            id="standard-select-currency"
+                            select
+                            label="Time"
+                            value={this.state.time}
+                            onChange={this.handleChangeTime}
+                            helperText="Please select your time">
+                            {this
+                              .state
+                              .timing
+                              .map((option) => (
+                                <MenuItem key={option.value} value={option.value}>
+                                  {option.label}
+                                </MenuItem>
+                              ))}
+                          </TextField>
+                        </div>
                       </Menu>
                       <IconButton>
                         <PersonAddIcon/>
@@ -236,16 +381,9 @@ class Dashboard extends Component {
                         <ColorLensIcon/>
                       </IconButton>
                       <IconButton>
-                        <input
-                          id="imageUpload"
-                          style={{
-                          display: 'none'
-                        }}
-                          type="file"
-                          onClick={alert("click")}
-                          />
-                          <AddPhotoAlternateIcon onClick={(e) => this.imageUpload.click() }/>
-                        
+
+                        <AddPhotoAlternateIcon/>
+
                       </IconButton>
                       <IconButton>
                         <ArchiveIcon/>
@@ -270,13 +408,14 @@ class Dashboard extends Component {
                         keepMounted
                         open={this.state.MoreMenuOpen}
                         onClose={this.moreMenuHandler}>
+
                         <MenuItem>Add Lable</MenuItem>
                         <MenuItem>Add Drawing</MenuItem>
                         <MenuItem>Show checkboxes</MenuItem>
                       </Menu>
                     </div>
                     <div className="card_buttonsRight">
-                      <Button>
+                      <Button onClick={this.addNote}>
                         close
                       </Button>
                     </div>
@@ -284,6 +423,15 @@ class Dashboard extends Component {
                 </CardActions>
               </Card>
             </div>
+            <div className="notes">
+              {this
+                .state
+                .allNotes
+                .map(objNote => {
+                  return (< Note noteData={objNote}/>)
+                })}
+            </div>
+
           </div>
 
         </div>
