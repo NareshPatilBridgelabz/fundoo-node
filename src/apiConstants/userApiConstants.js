996d58644c660f6f5eae5ec171888d4cc65f8c60
Naresh28696 <nareshpatil.nrp@gmail.com>
2020-04-01 17:14:55 +0530

    [Naresh] Refactore. Added new api constants

--- src/apiConstants/userApiConstants.js
+++ src/apiConstants/userApiConstants.js
@@ -2,5 +2,8 @@ module.exports = {
     singUp:"userSignUp",
     login:"login",
     getUser:"user_find",
-    forgotPassword:"reset"
+    forgotPassword:"reset",
+    getNotes:"getNotesList",
+    setNotes:"addNotes",
+    deleteNote:"trashNotes"
 }
