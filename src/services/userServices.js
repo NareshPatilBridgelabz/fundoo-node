03e4da8e8515b9e7a4cab2a5cb464e20493a7e8e
Naresh28696 <nareshpatil.nrp@gmail.com>
2020-04-13 15:29:16 +0530

    [Naresh] Added. add services for collaborators

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,5 +1,28 @@
 import axios from "axios";
 import userApiConstants from "../apiConstants/userApiConstants";
+let userData = JSON.parse(localStorage.getItem("userDetails"))
+
+export async function getUserData(){
+    try{
+        const response = await axios.get(process.env.REACT_APP_API_URL+'user');
+        return response;
+    }catch(err){
+        throw err;
+    }
+}
+
+export async function searchUserByWord(data){
+    try{
+        let sendData = {searchWord:data}
+        const response = await axios.post(process.env.REACT_APP_BASE_URL+ userApiConstants.SearchUserList,sendData,{
+            headers: {
+                Authorization:userData.id
+        }});
+        return response;
+    }catch(err){
+        throw err;
+    }
+}
 
 export async function register(registerData){
     try{
@@ -15,6 +38,7 @@ export async function login(loginData){
         const response = await axios.post(process.env.REACT_APP_BASE_URL + userApiConstants.login, loginData);
         localStorage.setItem('token', response.data.id)
         localStorage.setItem('userDetails', JSON.stringify(response.data))
+        localStorage.setItem('userProfileImage', JSON.stringify(response.data.imageUrl))
         return response;
     } catch(err) {
         console.log(err);
@@ -35,6 +59,18 @@ export async function dashboard(){
 export async function forgotPassword(data){
     try{
         const response = await axios.post(process.env.REACT_APP_BASE_URL + userApiConstants.forgotPassword,data);
+        return response;
+    } catch (err){
+        return err;
+    }
+}
+
+export async function uploadUserProfile(data){
+    try{
+        const response = await axios.post(process.env.REACT_APP_BASE_URL + userApiConstants.UploadProfileImage,data,{
+            headers: {
+                Authorization:userData.id
+        }});
         return response;
     } catch (err){
         return err;
