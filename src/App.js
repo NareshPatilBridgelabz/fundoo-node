d5d3cc1f37854d54cb9de378e583fd01f8b7c1e4
Naresh28696 <nareshpatil.nrp@gmail.com>
2020-04-16 16:42:11 +0530

    [Naresh] fix rendering issue

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import './App.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Switch } from 'react-router-dom'
 
+
 import Registration from './components/registration'
 import Login from './components/login'
 import Dashboard from './components/dashboard'
 import ForgotPassword from './components/forgotPassword'
 import FundooServices from './components/fundooSevices'
+import { UserProvider } from './components/userContext.js'
 
 class App extends React.Component {
   render () {
     return (
       <Router>
+        <UserProvider>
         <Switch>
+          
           <Route path='/registration' component={Registration} />
           <Route path='/login' component={Login} />
           <Route path='/dashboard' component={Dashboard} />
           <Route path='/forgotpassword' component={ForgotPassword} />
-          <Route path="/fundooServices" component={FundooServices} />
+          <Route path='/fundooServices' component={FundooServices} />
+          
         </Switch>
+        </UserProvider  >
       </Router>
     )
   }
