63adffb59c5e868c9e2b404577db67291433f1b0
Naresh28696 <nareshpatil.nrp@gmail.com>
2020-04-27 18:40:22 +0530

    [Naresh] Added constans for cart apis

--- src/apiConstants/userApiConstants.js
+++ src/apiConstants/userApiConstants.js
@@ -14,5 +14,6 @@ module.exports = {
     deleteForeverNotes:"deleteForeverNotes",
     GetNoteLabelList:'getNoteLabelList',
     UploadProfileImage:'uploadProfileImage',
-    SearchUserList:'searchUserList'
+    SearchUserList:'searchUserList',
+    Productcarts:'productcarts'
 }
