a471e72281fea19963bfadf1b7ae38b653d4b535
Naresh28696 <nareshpatil.nrp@gmail.com>
2020-04-27 18:37:12 +0530

    [Naresh] Added test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import {shallow} from 'enzyme'
+import Registration from './components/registration'
+import Login from './components/login'
+import ForgotPassword from './components/forgotPassword'
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('add',() => {
+  expect(true).toBe(true)
+})
+
+test('Registration Tets',() => {
+  
+  const { getByText } = render(<Registration />);
+  expect(getByText(/Submit/i)).toBeInTheDocument();
+})
+
+test('Login Tets',() => {
+  const { getByText } = render(<Login />);
+  expect(getByText(/login page/i)).toBeInTheDocument();
+})
+
+test('Forgot password Tets',() => {
+  const { getByText } = render(<ForgotPassword />);
+  expect(getByText(/Forgotpassword/i)).toBeInTheDocument();
+})
