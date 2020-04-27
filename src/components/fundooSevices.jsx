01921cc277ba67d20e939b0fcddeb16974a977a0
Naresh28696 <nareshpatil.nrp@gmail.com>
2020-04-27 18:28:22 +0530

    [Naresh] changed css

--- src/components/fundooSevices.jsx
+++ src/components/fundooSevices.jsx
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
 import Grid from '@material-ui/core/Grid'
-import { Row, Col } from 'react-grid-system'
 import Typography from '@material-ui/core/Typography'
 
 class FundooService extends Component {
+
+  constructor(props){
+    super(props)
+    this.state={
+      serviceName:''
+    }
+  }
+
+  loginHandler = ()=>{
+    this.props.history.push('./login')
+  }
+
+  registerHandler = (event)=>{
+    let service=event.currentTarget.getAttribute('service');
+    this.props.history.push({pathname :'./registration',data:{
+      service:service
+    }})
+  }
   render () {
     return (
       <div>
@@ -12,10 +29,9 @@ class FundooService extends Component {
         </div>
         <h2>fundooNotes offered. Choose below service to Register.</h2>
 
-        <Grid container xs={12}>
-          <Grid item xs></Grid>
-          <Grid item xs>
-            <div className='cardbox' id="card1">
+        <div className='card_container'>
+          <Grid >
+            <div className='cardbox' id="card1" service="advance" onClick={this.registerHandler}>
               <div className='services_card front_card '>
                 <Typography variant='h4'>price: $99 per month</Typography>
                 <Typography style={{color:"blue"}}>advance</Typography>
@@ -30,8 +46,8 @@ class FundooService extends Component {
               <div className='services_card back_card'>ADD TO CART</div>
             </div>
           </Grid>
-          <Grid item xs>
-            <div className='cardbox' id="card2">
+          <Grid >
+            <div className='cardbox' id="card2" service="basic" onClick={this.registerHandler}>
               <div className='services_card front_card'>
                 <Typography variant='h4'>price: $49 per month</Typography>
                 <Typography style={{color:"blue"}}>basic</Typography>
@@ -43,10 +59,10 @@ class FundooService extends Component {
               <div className='services_card back_card'>ADD TO CART</div>
             </div>
           </Grid>
-          <Grid item xs></Grid>
-        </Grid>
+        </div>
         <div className="header_footer">
-            <a href="" >click instead </a>
+            <span className='fontWeight-600'>click for </span>
+            <span className='fontWeight-600 blueFont cursor' onClick={e => this.props.history.push('/login')} >login </span>
         </div>
       </div>
     )
